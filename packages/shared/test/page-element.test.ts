3d0c81a686f65c7d1fa51b51e7155079a4decfac
FlowerF <1647231392@qq.com>
2022-06-08 23:43:09 +0800

    chore: 安装与使用vitest

---
+++ packages/shared/test/page-element.test.ts
@@ -0,0 +1,30 @@
+import { describe, expect, it, test } from 'vitest';
+import { Project } from '../src';
+
+describe('page element', () => {
+  const project = Project.create();
+
+  test('project.name', () => {
+    expect(project.name).toBe('New Project');
+  });
+
+  test('project.page.length === 1', () => {
+    expect(project.pages.length).toBe(1);
+  });
+
+  test('project.getJson', () => {
+    expect(project.getJson()).toMatchInlineSnapshot(`
+      {
+        "description": "New Project Description",
+        "name": "New Project",
+        "pages": [
+          {
+            "description": "New Page Description",
+            "elements": [],
+            "name": "New Page",
+          },
+        ],
+      }
+    `);
+  });
+});
